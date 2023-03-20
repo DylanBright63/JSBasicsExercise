3a45f11052f5aeafb1c8ca41d20f1466a98f63e2
Student <Brightdylan93@gmail.com>
2023-03-19 20:13:45 -0500

    finshed

---
+++ script.js
@@ -0,0 +1,20 @@
+console.log(63);
+
+// Boolean, string, number, null, undefined
+
+// +, -, *, /, **, %
+
+console.log(5 % 2);
+console.log(5 % 4);
+
+console.log('I\'M MARRY POPPINS Y\'ALL');
+
+
+console.log("HAWK" + "eye");
+
+
+console.log("Dylan".length + "Bright".length)
+
+
+
+"Avengers: Endgame"[16];
